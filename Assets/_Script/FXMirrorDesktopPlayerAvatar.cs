f2e12b1740bbf147a3b1fa00f95e16d416676545
Raditya Ari Pradana <54885169+radityaari10@users.noreply.github.com>
2025-05-08 10:05:57 +0700

    Update for Mobile Player
    Activate UICanvasControllerInput for Mobile Player

--- Assets/_Script/FXMirrorDesktopPlayerAvatar.cs
+++ Assets/_Script/FXMirrorDesktopPlayerAvatar.cs
@@ -2,6 +2,7 @@ using Cinemachine;
 using StarterAssets;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
 
 public class FXMirrorDesktopPlayerAvatar : FXMirrorPlayerAvatar
 {
@@ -36,6 +37,22 @@ public class FXMirrorDesktopPlayerAvatar : FXMirrorPlayerAvatar
 		{
 			assetsInputs.enabled = true;
 			Debug.Log("Enabled assetsInputs");
+
+			UICanvasControllerInput uICanvasControllerInput = FindAnyObjectByType<UICanvasControllerInput>();
+			if (uICanvasControllerInput != null)
+			{
+				if ((Application.platform == RuntimePlatform.Android && !XRSettings.enabled) || Application.platform == RuntimePlatform.IPhonePlayer || Application.isEditor) 
+				{ 	
+					uICanvasControllerInput.gameObject.SetActive(true);
+				}
+				else
+				{
+					uICanvasControllerInput.gameObject.SetActive(false);
+				}
+
+				uICanvasControllerInput.starterAssetsInputs = assetsInputs;
+				Debug.Log("Mobile UI Canvas set");
+			}
 		}
 
 		if (playerInput != null)
